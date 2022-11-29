10f0eeaf648dfd775de728530a99fdd97f874a14
Gosia Borowska <malgorzata.x.borowska@gsk.com>
2022-11-29 16:11:40 +0100

    initial commit

---
+++ IceCreamRatingsApiProject/Rating.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IceCreamRatingsApiProject
+{
+    public class Rating
+    {
+        public string userId;
+        public string productId;
+        public string locationName;
+        public int rating;
+        public string userNotes;
+    }
+}
