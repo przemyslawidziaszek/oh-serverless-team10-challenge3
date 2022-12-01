a1c47451ba425ce23f0d57c8bb2beeaacd175dd0
Przemyslaw Idziaszek <przemyslaw.idziaszek@gmail.com>
2022-12-01 09:35:15 +0100

    GetRating method added

---
+++ IceCreamRatingsApiProject/GetRating.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IceCreamRatingsApiProject
+{
+    public static class GetRating
+    {
+        [FunctionName("GetRating")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "GetRating")]
+            HttpRequest req,
+            [Sql("SELECT [id],[userId],[productId],[timestamp],[locationName],[rating],[userNotes] FROM [ICECREAM].[ratings] where Id = @ratingId",
+                CommandType = System.Data.CommandType.Text,
+                Parameters = "@ratingId={Query.ratingId}",
+                ConnectionStringSetting = "MSSQL_CONSTR")]
+            IEnumerable<Rating> ratingItem)
+        {
+            return new OkObjectResult(ratingItem.FirstOrDefault());
+        }
+    }
+}
